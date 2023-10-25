44bb5b93d87837657f47577fbaa0f99d246e5e3a
drik-exe <27danik98@gmail.com>
2023-10-25 21:53:21 +0300

    started users app, connected products.html.

--- django_auction/goods/urls.py
+++ django_auction/goods/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
+from goods.views import GoodsView
 
 app_name = 'goods'
 
 
 urlpatterns = [
+    path('', GoodsView.as_view(), name='goods'),
 
 ]
--- django_auction/goods/views.py
+++ django_auction/goods/views.py
@@ -4,3 +4,7 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = 'goods/index.html'
+
+
+class GoodsView(TemplateView):
+    template_name = 'goods/products.html'
